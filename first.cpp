e43b990d4a8b193b378d8e743f6933bbb1e37a41
Arun Kumar <arun@example.com>
2026-04-30 10:22:15 +0530

    define local and global variable

--- first.cpp
+++ first.cpp
@@ -1,12 +1,30 @@
 //temprature conversion
 // Take input in the form of calcius and convert it into fahrenheit
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     double calcius;
+//     double fahrenheit;
+//     cin>>calcius;
+//     fahrenheit = (calcius * 9.0/5.0) + 32.0;
+//     cout<<fahrenheit<<endl;
+//     return 0; 
+// }
+
+
+// Global and local variables
 #include <iostream>
 using namespace std;
+
+//global variable
+int globalVariable = 100;
 int main(){
-    double calcius;
-    double fahrenheit;
-    cin>>calcius;
-    fahrenheit = (calcius * 9.0/5.0) + 32.0;
-    cout<<fahrenheit<<endl;
-    return 0; 
+    //local variable
+    int userInput;
+    float result;
+    cin>>userInput;
+    result = (double)globalVariable / userInput;
+    cout<<result<<endl;
+    return 0;
+
 }
