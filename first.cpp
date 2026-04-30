fd6e6c6c0af818a2abd3726fdaaa9d06482a84de
Arun Kumar <arun@example.com>
2026-04-30 10:34:33 +0530

    logical and relational oprerator

--- first.cpp
+++ first.cpp
@@ -12,19 +12,33 @@
 // }
 
 
-// Global and local variables
+// // Global and local variables
+// #include <iostream>
+// using namespace std;
+
+// //global variable
+// int globalVariable = 100;
+// int main(){
+//     //local variable
+//     int userInput;
+//     float result;
+//     cin>>userInput;
+//     result = (double)globalVariable / userInput;
+//     cout<<result<<endl;
+//     return 0;
+
+// }
+
+
+
+// logical and relatonal operators
 #include <iostream>
 using namespace std;
 
-//global variable
-int globalVariable = 100;
 int main(){
-    //local variable
-    int userInput;
-    float result;
-    cin>>userInput;
-    result = (double)globalVariable / userInput;
-    cout<<result<<endl;
+    int side1, side2, side3;
+    cin>>side1>>side2>>side3;
+    bool isEqui = (side1 > 0) && (side1 == side2) && (side2 == side3);
+    cout<<isEqui<<endl;
     return 0;
-
 }
