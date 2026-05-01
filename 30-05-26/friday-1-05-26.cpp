b59434a0cfffcda36c1ac8c6df55a6c6115f6ebc
Arun Kumar <arunprajapat629@gmail.com>
2026-05-01 09:43:13 +0530

    ....

--- 30-05-26/friday-1-05-26.cpp
+++ 30-05-26/friday-1-05-26.cpp
@@ -1,16 +1,76 @@
 //problem 1  if condition
 // to verify user's age wether eligible for vote or not ?
+// #include <iostream>
+// using namespace std;
+
+// int main (){
+//     int age;
+//     cin>>age;
+//     //if condition
+//     if(age >= 18){
+//         //logic
+//         cout<<"Eligible for vote"<<endl;
+//     }
+//     return 0;
+
+// }
+
+
+
+
+
+
+
+
+
+
+// Problem 2 -- If - else condition 
+// to verify user's age wether eligible for vote or not ?
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int age;
+//     cin >> age;
+//     // if condition 
+//     if(age >= 18) {
+//         // logic 
+//         cout << "Eligible For vote";
+//     } else {
+//         // logic 
+//         cout << "Not Eligible For vote";
+//     }
+//     return 0;
+// }
+
+
+
+
+
+
+
+
+//program 3
+//to verify user's age wether eligible for vote or not ?
 #include <iostream>
 using namespace std;
 
-int main (){
+int main(){
     int age;
     cin>>age;
-    //if condition
     if(age >= 18){
-        //logic
-        cout<<"Eligible for vote"<<endl;
+        if(age >= 100){
+            cout<<"Eligible for vote (but You are a super senior citizen)"<<endl;
+        }else{
+            cout<<"Eligible for vote"<<endl;
+        }
+    }else{
+        if(age < 0){
+            cout<<"Invalid age"<<endl;
+        }else{
+            cout<<"Not eligible for vote"<<endl;
+        }
     }
-    return 0;
 
+    return 0;
 }
