5a70b4e7fce4ba84e975eec89cb67f106c055601
Arun Kumar <arunprajapat629@gmail.com>
2026-05-01 10:02:17 +0530

    ....

--- 30-05-26/day-2.cpp
+++ 30-05-26/day-2.cpp
@@ -52,25 +52,53 @@
 
 //program 3
 //to verify user's age wether eligible for vote or not ?
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     int age;
+//     cin>>age;
+//     if(age >= 18){
+//         if(age >= 100){
+//             cout<<"Eligible for vote (but You are a super senior citizen)"<<endl;
+//         }else{
+//             cout<<"Eligible for vote"<<endl;
+//         }
+//     }else{
+//         if(age < 0){
+//             cout<<"Invalid age"<<endl;
+//         }else{
+//             cout<<"Not eligible for vote"<<endl;
+//         }
+//     }
+
+//     return 0;
+// }
+
+
+
+
+
+
+
+
+
+
+
+// problem 4    
+// find the greater number among 2
+
 #include <iostream>
 using namespace std;
 
 int main(){
-    int age;
-    cin>>age;
-    if(age >= 18){
-        if(age >= 100){
-            cout<<"Eligible for vote (but You are a super senior citizen)"<<endl;
-        }else{
-            cout<<"Eligible for vote"<<endl;
-        }
-    }else{
-        if(age < 0){
-            cout<<"Invalid age"<<endl;
-        }else{
-            cout<<"Not eligible for vote"<<endl;
-        }
-    }
+    int a, b;
+    int max_number;
+    cin>>a>>b;
+
+    //ternary operator
+    max_number = (a > b) ? a : b;
+    cout<<"Greater number is: "<<max_number<<endl;
 
     return 0;
 }
