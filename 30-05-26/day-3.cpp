049fd4840ab0679106213a19df7b396a7fea22aa
Arun Kumar <arunprajapat629@gmail.com>
2026-05-02 09:40:43 +0530

    ....

--- 30-05-26/day-3.cpp
+++ 30-05-26/day-3.cpp
@@ -2,11 +2,25 @@
 // print 1 to 10 using for loop
 
 
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     for(int i=1; i<=10; i++){
+//         cout<<i<<" ";
+//     }
+//     return 0;
+// }
+
+
+// problem 2
+// print 10 to 1  using for loop
+
 #include<iostream>
-using namespace std;
+using namespace std;    
 
 int main(){
-    for(int i=1; i<=10; i++){
+    for(int i=10; i>=1; i--){
         cout<<i<<" ";
     }
     return 0;
