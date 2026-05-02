eda8d0c9f046906aec68df0815ed9e5d6b64c513
Arun Kumar <arunprajapat629@gmail.com>
2026-05-02 09:49:29 +0530

    ....

--- 30-05-26/day-3.cpp
+++ 30-05-26/day-3.cpp
@@ -16,12 +16,52 @@
 // problem 2
 // print 10 to 1  using for loop
 
+// #include<iostream>
+// using namespace std;    
+
+// int main(){
+//     for(int i=10; i>=1; i--){
+//         cout<<i<<" ";
+//     }
+//     return 0;
+// }
+
+
+
+
+
+// problem 3
+// print 1 to 20 using while loop
+
+// #include<iostream>
+// using namespace std;    
+
+// int main(){
+//     int i=1;
+//     while(i<=20){
+//         cout<<i<<" ";
+//         i++;
+//     }
+//     return 0;
+// }
+
+
+
+
+
+
+//problem 4
+// print 20 to 1 using while loop
+
+
 #include<iostream>
 using namespace std;    
 
 int main(){
-    for(int i=10; i>=1; i--){
+    int i=20;
+    while(i>=1){
         cout<<i<<" ";
+        i--;
     }
     return 0;
 }
