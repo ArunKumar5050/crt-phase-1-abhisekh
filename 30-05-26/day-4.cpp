a00a280c815094cf8d2684db1a30c90371a0b798
Arun Kumar <arunprajapat629@gmail.com>
2026-05-04 11:29:24 +0530

    ....

--- 30-05-26/day-4.cpp
+++ 30-05-26/day-4.cpp
@@ -106,16 +106,43 @@
 
 //problem 5
 //print a rectangle
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int row, col;
+//     cin >> row >> col;
+//     for (int i = 1; i <= row; i++){
+//         for (int j = 1; j <= col; j++){
+//             cout << "* ";
+//         }
+//         cout << endl;
+//     }
+// }
+
+
+
+
+
+
+
+
+
+//program 6
+
 #include<iostream>
 using namespace std;
 
 int main(){
-    int row, col;
-    cin >> row >> col;
-    for (int i = 1; i <= row; i++){
-        for (int j = 1; j <= col; j++){
-            cout << "* ";
+    for(int i=1; i<=5; i++){
+        for(int j=1; j<=5; j++){
+            if(i==1 || i==5 || j==1 || j==5){
+                cout<<"* ";
+            }else{
+                cout<<"  ";
+            }
+
         }
-        cout << endl;
+        cout<<endl;
     }
 }
