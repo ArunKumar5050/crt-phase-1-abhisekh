cbca2085322605c30cd7987791ca61b44b7ae51c
Arun Kumar <arunprajapat629@gmail.com>
2026-05-04 09:53:57 +0530

    ....

--- 30-05-26/day-4.cpp
+++ 30-05-26/day-4.cpp
@@ -1,19 +1,66 @@
 //problem 1
 //print1 to 10 , 10 number of times
 
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     for (int i = 0; i <= 10; i++)
+//     {
+//         for (int j = 1; j <= 10; j++)
+//         {
+//             cout << j << " ";
+//         }
+//         cout << endl;
+//     }
+    
+// }
+
+
+
+
+
+
+
+
+
+
+
+// problem 2
+//print a solid squre
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     for (int i = 0; i < 5; i++)
+//     {
+//         for (int j = 0; j < 5; j++)
+//         {
+//             cout << "* ";
+//         }
+//         cout << endl;
+//     }
+    
+// }
+
+
+
+
+
+
+
+
+//problem 3
+//print right angle triangle
 #include<iostream>
 using namespace std;
 int main(){
-    for (int i = 0; i <= 10; i++)
+    for (int i = 1; i <= 5; i++)
     {
-        for (int j = 1; j <= 10; j++)
+        for (int j = 1; j <= i; j++)
         {
-            cout << j << " ";
+            cout << "* ";
         }
         cout << endl;
     }
     
 }
-
-
 
