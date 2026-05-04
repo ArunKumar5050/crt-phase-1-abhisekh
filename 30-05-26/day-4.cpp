d44b979cf3248e48aece698b044e94d71930fae6
Arun Kumar <arunprajapat629@gmail.com>
2026-05-04 11:51:45 +0530

    ....

--- 30-05-26/day-4.cpp
+++ 30-05-26/day-4.cpp
@@ -129,19 +129,40 @@
 
 
 //program 6
+//print hollow square
+// #include<iostream>
+// using namespace std;
 
+// int main(){
+//     for(int i=1; i<=5; i++){
+//         for(int j=1; j<=5; j++){
+//             if(i==1 || i==5 || j==1 || j==5){
+//                 cout<<"* ";
+//             }else{
+//                 cout<<"  ";
+//             }
+
+//         }
+//         cout<<endl;
+//     }
+// }
+
+
+
+//program 7
+//print a pyramid
 #include<iostream>
 using namespace std;
-
 int main(){
-    for(int i=1; i<=5; i++){
-        for(int j=1; j<=5; j++){
-            if(i==1 || i==5 || j==1 || j==5){
-                cout<<"* ";
-            }else{
-                cout<<"  ";
-            }
-
+    int n;
+    cout<<"Enter the number of rows: ";
+    cin>>n;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n-i; j++){
+            cout<<"  ";
+        }
+        for(int k=1; k<=2*i-1; k++){
+            cout<<"* ";
         }
         cout<<endl;
     }
